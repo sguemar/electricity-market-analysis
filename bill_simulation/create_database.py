7241ec8cf5e84909ba068a18935d3c7a80625867
Samuel <samuguemar@gmail.com>
2020-01-10 16:54:22 +0000

    Users username field must be unique

--- bill_simulation/create_database.py
+++ bill_simulation/create_database.py
@@ -22,7 +22,7 @@ cursor.execute(
     CREATE TABLE IF NOT EXISTS users
       (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) NOT NULL,
+        username VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
         type TINYINT NOT NULL
       );
