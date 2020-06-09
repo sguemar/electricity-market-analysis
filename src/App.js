740930dd6caf546a9569b218232357057ea97c06
Samuel <samuguemar@gmail.com>
2020-06-09 13:45:45 +0100

    Router and 404 error page added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
